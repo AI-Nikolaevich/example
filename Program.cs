c7d4f597db5a74d7ffb2d01c13031d2b2796d205
AI-Nikolaevich <ilia.antukhov@yandex.ru>
2026-04-01 16:08:22 +0300

    git ignore

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Пробуем всё что можно");
+    c.RoutePrefix = string.Empty;
+});
+
+
+app.MapGet("/test", () => "Hello World!");
 
 app.Run();
